e8333c277017fe2cfec10fc6e84d05e28a7c47a5
David Mojica <davidmojicav@gmail.com>
2023-12-30 12:10:01 -0500

    EMC

--- app/app2.ts
+++ app/app2.ts
@@ -1,1 +1,7 @@
 console.log('App2');
+//Listas
+let lista:string[] = ["Pepe1", "Pedro", "huevon"];
+
+for (const i of lista){
+    console.log(i);
+}
--- app/build/bundle.js
+++ app/build/bundle.js
@@ -10,5 +10,14 @@ console.log("Typescript");
 //tsconfig -> outDir:  "./build"
 //Para no darle sobrecarga al servidor, hay que evitar llamar muchos archivos scripts.  La solucion es outFile
 //tsconfig ->outFile: ".build/bundle.js". "module" : "none"
+//Incluir y excluir modulos
+//Usar include y exclude en el tsconfig luego tsc
+//Para cambiar el tipo de salida en el bundle, se puede configurar el emascript
+//"target": "ES6",   "target": "ES5", etc
 console.log('App2');
+//Listas
+let lista = ["Pepe1", "Pedro", "huevon"];
+for (const i of lista) {
+    console.log(i);
+}
 console.log('app3');
