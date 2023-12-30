ddddaf096fabfb6b4358d58c1b70459fa212da2b
David Mojica <davidmojicav@gmail.com>
2023-12-30 11:53:38 -0500

    Bundle

--- build/bundle.js
+++ build/bundle.js
@@ -1,15 +1,14 @@
+"use strict";
 console.log("Typescript");
 //Para ejecutar TS hay que compilar el archivo mediante la consola.
 //tsc app.ts
-
 //Modo observador: Reflejar automaticamente los cambios en el archivo TS.
 //tsc app -w
-
 //Inicializar el archivo tsconfig
 //tsc --init
-
 //Seleccionar ruta de salida para la compilación de archivos:
 //tsconfig -> outDir:  "./build"
-
 //Para no darle sobrecarga al servidor, hay que evitar llamar muchos archivos scripts.  La solucion es outFile
 //tsconfig ->outFile: ".build/bundle.js". "module" : "none"
+console.log('App2');
+console.log('app3');
